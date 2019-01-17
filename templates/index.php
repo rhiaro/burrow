b5d04f91ddd59598d1475de4b4d349a413cef80e
rhiaro <amy@rhiaro.co.uk>
2019-01-17 20:01:54 +0200

    JS date reloader

--- templates/index.php
+++ templates/index.php
@@ -13,6 +13,15 @@
         max-height: 300px;
         overflow: scroll;
       }
+      #reload {
+        display: inline-block;
+        width: 1em; height: 1em;
+        border-radius: 100%;
+        background-color: violet;
+      }
+      #reload:hover {
+        cursor: pointer;
+      }
     </style>
   </head>
   <body>
@@ -61,8 +70,9 @@
               <option value="<?=date("d", strtotime("2016-01-$i"))?>"<?=(date("j") == $i) ? " selected" : ""?>><?=date("d", strtotime("2016-01-$i"))?></option>
             <?endfor?>
           </select>
-          <input type="text" name="time" id="time" value="<?=date("H:i:s")?>" />
-          <input type="text" name="zone" id="zone" value="<?=date("P")?>" />
+          <input type="text" name="time" id="time" value="<?=date("H:i:s")?>" size="8" />
+          <input type="text" name="zone" id="zone" value="<?=date("P")?>" size="5" />
+          <span id="reload"></span>
         </p>
         <hr/>
         <!-- temp -->
@@ -111,5 +121,51 @@
       <?endif?>
       </p>
     </footer>
+    <script>
+
+      var reloadBtn = document.getElementById('reload');
+      reloadBtn.addEventListener('click', function(e){
+        var now = new Date();
+        var year = now.getFullYear().toString();
+        var month = now.getMonth()+1;
+        month = month.toString().padStart(2, '0');
+        var day = now.getDate();
+        day = day.toString().padStart(2, '0');
+        var time = now.getHours().toString().padStart(2, '0') + ':' + now.getMinutes().toString().padStart(2, '0') + ':' + now.getSeconds().toString().padStart(2, '0');
+        var zoneDiff = now.getTimezoneOffset() / 60;
+        if(zoneDiff <= 0){
+          var sign = '+';
+        }else{
+          var sign = '-';
+        }
+        var zone = sign + Math.abs(zoneDiff).toString().padStart(2, '0') + ':00';
+
+        var yearEles = document.getElementById('year').getElementsByTagName('option');
+        var monthEles = document.getElementById('month').getElementsByTagName('option');
+        var dayEles = document.getElementById('day').getElementsByTagName('option');
+
+        for(var i = 0; i < yearEles.length; i=i+1){
+          if(yearEles[i].value == year){
+            yearEles[i].selected = 'true';
+          }
+        }
+        for(var i = 0; i < monthEles.length; i=i+1){
+          if(monthEles[i].value == month){
+            monthEles[i].selected = 'true';
+          }
+        }
+        for(var i = 0; i < dayEles.length; i=i+1){
+          if(dayEles[i].value == day){
+            dayEles[i].selected = 'true';
+          }
+        }
+
+        document.getElementById('time').value = time;
+        document.getElementById('zone').value = zone;
+
+      });
+
+      
+    </script>
   </body>
 </html>
